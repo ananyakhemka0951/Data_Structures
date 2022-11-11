8940b0c1a2e074c3ce236ff2d552008b7005d3ce
ananyakhemka0951 <115224852+ananyakhemka0951@users.noreply.github.com>
2022-11-11 19:24:53 +0530

    Update test1(3).c

--- test1(3).c
+++ test1(3).c
@@ -8,8 +8,42 @@ int * blah(int val) {
 	return pointer;
 }
 
+int alloc() {
+    int * a;
+    int i, n, val;
+
+    n = 3;
+    a = malloc(n * sizeof(int));
+
+    // enter data
+    i = 0;
+    scanf("%d", &val);
+    while(val>0){
+        a[i] = val;
+        i = i+1;
+        if(i>=n) {
+            n = n*2;
+            a = realloc(a, n * sizeof(int));
+        }
+        scanf("%d", &val);
+    }
+    n = i;
+    a = realloc(a, n * sizeof(int));
+
+    // print data
+    for(i=0; i<n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n\nDone!\n");
+    
+    return 0;
+}
+
 int main(int argc, char **argv) {
 	int * b = blah(5);
 	printf("%d.\n", *b);
+	
+	int n=alloc();
 	return 0;
 }
