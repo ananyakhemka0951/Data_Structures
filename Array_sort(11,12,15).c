7422dc6ea678fbfbc4dd046da1764074a952ccdc
ananyakhemka0951 <115224852+ananyakhemka0951@users.noreply.github.com>
2022-11-13 19:01:14 +0530

    Update Array_sort(11,12,15).c

--- Array_sort(11,12,15).c
+++ Array_sort(11,12,15).c
@@ -11,7 +11,7 @@ void selectionSort(int arr[], int n) {
 	int imin;
 
 	// Outer iteration loop
-    for (int i = 0; i < n-1; i++) // As j will start from i+1, thus i < n-1. 
+    for (int i = 0; i < n-1; i++)
     {
         // Inner loop to find the minimum element in the array from i to n
         imin = i;
@@ -23,6 +23,7 @@ void selectionSort(int arr[], int n) {
         // Swap the found minimum element with the first element
         if(imin != i)
 		{
+		// swapping the minimum and placing it at its correct position i.e. i-th position
 		int temp = arr[imin];
         arr[imin] = arr[i];
         arr[i] = temp;
@@ -34,6 +35,59 @@ void selectionSort(int arr[], int n) {
 
 
 
+
+/* ================================================== */
+//The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+void insertionSort(int arr[], int n)
+{
+	int j;
+    for (int i = 1; i < n; i++)//inner loop to iterate through the array from 1st position to last as the inner loop will start from i-1 {
+		int compare=arr[i];
+        for(j=i-1; j>=0 && arr[j]>compare; j--) //iteration to compare arr[i] with elements before arr[i], Thus j=i-1
+		{
+            arr[j + 1] = arr[j];  // moving elements of the array that are greater that wall to one position ahead of their current position
+        }
+        arr[j] = compare;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/* ================================================== */
+//QUICK SORT: The array is virtually split into a sorted and an unsorted part. Values f
+
+int partition(int *a, int leftmost, int rightmost){
+  //assign pivot index to thre rightmost element of the array  
+  int pivot = a[rightmost];
+  int ind = leftmost;
+  for (int i = leftmost; i < rightmost; i++){
+      if(a[i] <= pivot){
+          int temp = a[i];
+          a[i] = a[pIndex];
+          a[ind] = temp;
+          ind++;
+      }
+  }
+  int temp = a[ind];
+  a[ind] = a[rightmost];
+  a[rightmost] = temp;
+  return pIndex;
+}
+
+void quickSort(int *a, int leftmost, int rightmost){
+    if(a && leftmost < rightmost){
+        int pIndex = partition(a, leftmost, rightmost);
+        quickSort(a, leftmost, pIndex-1);
+        quickSort(a, pIndex+1, rightmost);
+    }
+}
 
 
 
@@ -49,6 +103,14 @@ void printArray(int arr[], int size)
     printf("\n");
 }
 
+//function to print the array with a pointer
+void printArray(int * a, int n) {
+	printf("Array = ");
+	for(int i=0; i<n; i++) {
+		printf( i?", %d":"%d", a[i]);
+	}
+	printf(".\n");
+}
 
 
 
@@ -58,6 +120,7 @@ void printArray(int arr[], int size)
 
 int main(int argc, char **argv) {
 	
+	//Selection sort
 	int n = 10;
 	int arr[n];
 
@@ -77,5 +140,47 @@ int main(int argc, char **argv) {
 	printArray(arr, n);
 
 
+
+	//Insertion sort
+	printf("Enter the elements of the array for insertion sort");
+	int a=0;
+	for (int i=0; i<n; i++)
+		{
+			scanf("%d",&a);
+			arr[i]=a;
+		}
+
+
+	printArray(arr, n);
+
+	insertionSort(arr, n);
+	printf("Insertion Sort:");
+	printArray(arr, n);
+
+
+	//Quick Sort
+	int * a;
+	  int n = 10;
+
+	  srand(time(NULL));
+
+	  a = generateArray(n);
+    printArray(a,n);
+    selectionSort(a, n);
+
+    printf("Sorted array: \n");
+    quickSort(a, 0, n-1);
+
+
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
