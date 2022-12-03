14757f34614d87b76abafcc4d3bff9fc2eec751a
ananyakhemka0951 <115224852+ananyakhemka0951@users.noreply.github.com>
2022-12-03 18:51:27 +0530

    Binary_tree(8,9)
    Added extra comments

--- Deliverables/Binary_tree(8,9).c
+++ Deliverables/Binary_tree(8,9).c
@@ -8,17 +8,24 @@ struct node
     struct node* right;
 };
 
+
+// creating a new data type NodeAddress
 typedef struct node* Nodeaddress;
 
+
+// Tp search the correct position of the element for insertion
 Nodeaddress search(Nodeaddress root, int val)
 {
     if(root == NULL)
     return NULL;
 
+    
+    //if the value is grater than the parent/root, we move to the right side of the tree
     if(val > root->val)
     {
         search(root->right, val);
     }
+    //If the valuye is less than the parent/root, we move to the left side of the tree
     else if(val < root->val )
     {
         search(root->left, val);
@@ -29,35 +36,44 @@ Nodeaddress search(Nodeaddress root, int val)
     
 }
 
-#To allocate space for a new node and assign the val to it.
+//To allocate space for a new node and assign the val to it.
 void create(Nodeaddress root, int val)
 {
-    root = malloc(sizeof(struct node)); #
+    root = malloc(sizeof(struct node)); // memory allocation for the node
     root->val = val;
 }
 
+
+//To insert every element in a form of a binary search tree
 Nodeaddress insert(Nodeaddress root, int val)
 {
-    #Case for the root of the tree
+    //Case for the root of the tree
     if(root == NULL)
     {
         create(root, val);
         return root;
     }
 
+    //if the value is grater than the parent/root, we move to the right side of the tree
     else if(val > root->val)
     {
+        //if there exists a node right of the parent/root, we recursively call insert again to campare it with the value on the right of the parent
         if(root->right)
             insert(root->right, val);
         else
+            //If the right node does not exist, we will call create to allocate space fopr that node and add the element to its right position
             create(root->right, val);
     }
 
+    
+    //if the value is greater than the parent/root, we move to the left side of the tree
     else if(val < root->val)
     {
+        //if there exists a node left of the parent/root, we recursively call insert again to campare it with the value on the left of the parent
         if(root->left)
             insert(root->left, val);
         else
+            //If the left node does not exist, we will call create to allocate space fopr that node and add the element to its left position
             create(root->left, val);
     }
 
@@ -104,6 +120,7 @@ int main()
         printf("%d, ", arr[i]);
     }
 
+    //case for the root of the binary search tree
     Nodeaddress root = malloc(sizeof(struct node));
 
     for(int i=0; i<n; i++)
